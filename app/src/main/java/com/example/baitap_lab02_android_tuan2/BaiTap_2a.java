d59ddcc1df993a99a3b7901c8c319fd90d3ffd0b
trung2702 <lekhactrung6448@gmail.com>
2021-03-29 09:47:35 +0700

    End-tuan2

---
+++ app/src/main/java/com/example/baitap_lab02_android_tuan2/BaiTap_2a.java
@@ -0,0 +1,14 @@
+package com.example.baitap_lab02_android_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaiTap_2a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai_tap_2a);
+    }
+}
---
+++ app/src/main/java/com/example/baitap_lab02_android_tuan2/test.java
@@ -0,0 +1,4 @@
+package com.example.baitap_lab02_android_tuan2;
+
+public class test {
+}
